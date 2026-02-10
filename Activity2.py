50dc21edb0d52f42f25c845de1a15ac5eb19fdb9
studentdino2 <gonzaga.dino2@gmail.com>
2026-02-10 18:10:22 +0800

    Add Activity2.py with personal information prints

---
+++ Activity2.py
@@ -0,0 +1,16 @@
+Fullname="Dino O Gonzaga II"
+Age = 19
+height= 1.70
+working = False
+
+print("Fullname",Fullname)
+print(type(Fullname))
+
+print("\nAge",Age)
+print(type(Age))
+
+print("\nHeight",height)
+print(type(height))
+
+print("\nWorking",working)
+print(type(working))
